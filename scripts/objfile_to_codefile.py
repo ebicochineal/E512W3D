a5bb8775c9995df61d1733789c1215e77da15c8a
ebicochineal <yz94588@gmail.com>
2022-11-25 14:40:02 +0900

    fix

--- scripts/objfile_to_codefile.py
+++ scripts/objfile_to_codefile.py
@@ -52,20 +52,85 @@ with open(p+obj_file_name+'.hpp', 'w') as f:
     
     f.write('#include "E512W3D.hpp"\nMesh %s;\nvoid %sInit () {\n'%(obj_file_name, obj_file_name))
     
+    f.write('    // %s.vertexs '%obj_file_name + str(len(vertex)) + '\n')
+    f.write('    // %s.faces '%obj_file_name + str(len(face)) + '\n')
+    f.write('    // %s.uv_vertexs '%obj_file_name + str(len(uvvertex)) + '\n')
+    f.write('    // %s.uv_faces '%obj_file_name + str(len(uvface)) + '\n')
+    
+    
+    f.write('#ifdef __EMSCRIPTEN__\n')
+    
+    f.write('    {\n')
+    f.write('        %s.vertexs.reserve('%obj_file_name + str(len(vertex)) + ');\n')
+    t = '        float va[] = {'
+    for x, y, z in vertex:
+        t += str(x) + ',' + str(y) + ',' + str(z) + ','
+    t += '};\n'
+    f.write(t)
+    f.write('        for (int i = 0; i < %d; i += 3) {\n'%(len(vertex)*3))
+    f.write('            %s.addVertex(va[i], va[i+1], va[i+2]);\n'%obj_file_name)
+    f.write('        }\n')
+    f.write('    }\n')
+    
+    f.write('    {\n')
+    f.write('        %s.faces.reserve('%obj_file_name + str(len(face)) + ');\n')
+    t = '        int fa[] = {'
+    for a, b, c in face:
+        t += str(a) + ',' + str(b) + ',' + str(c) + ','
+    t += '};\n'
+    f.write(t)
+    f.write('        for (int i = 0; i < %d; i += 3) {\n'%(len(face)*3))
+    f.write('            %s.addFace(fa[i], fa[i+1], fa[i+2]);\n'%obj_file_name)
+    f.write('        }\n')
+    f.write('    }\n')
+    
+    if len(uvface) > 0:
+        f.write('    {\n')
+        f.write('        %s.uv_vertexs.reserve('%obj_file_name + str(len(uvvertex)) + ');\n')
+        t = '        float uva[] = {'
+        for x, y in uvvertex:
+            t += str(x) + ',' + str(y) + ','
+        t += '};\n'
+        f.write(t)
+        f.write('        for (int i = 0; i < %d; i += 2) {\n'%(len(uvvertex)*2))
+        f.write('            %s.addVertexUV(uva[i], uva[i+1]);\n'%obj_file_name)
+        f.write('        }\n')
+        f.write('    }\n')
+        
+        
+        f.write('    {\n')
+        f.write('        %s.uv_faces.reserve('%obj_file_name + str(len(uvface)) + ');\n')
+        t = '        int ufa[] = {'
+        for a, b, c in uvface:
+            t += str(a) + ',' + str(b) + ',' + str(c) + ','
+        t += '};\n'
+        f.write(t)
+        f.write('        for (int i = 0; i < %d; i += 3) {\n'%(len(uvface)*3))
+        f.write('            %s.addFaceUV(ufa[i], ufa[i+1], ufa[i+2]);\n'%obj_file_name)
+        f.write('        }\n')
+        f.write('    }\n')
+    
+    f.write('#else\n')
+    
+    
     f.write('    %s.vertexs.reserve('%obj_file_name + str(len(vertex)) + ');\n')
     f.write('    %s.faces.reserve('%obj_file_name + str(len(face)) + ');\n')
-    if len(uvface) > 0:
-        f.write('    %s.uv_vertexs.reserve('%obj_file_name + str(len(uvvertex)) + ');\n')
-        f.write('    %s.uv_faces.reserve('%obj_file_name + str(len(uvface)) + ');\n')
     for i in vertex:
         f.write('    %s.addVertex('%obj_file_name + ', '.join(str(x) for x in i) + ');\n')
     for i in face:
         f.write('    %s.addFace('%obj_file_name + ', '.join(str(x) for x in i) + ');\n')
-    for i in uvvertex:
-        f.write('    %s.addVertexUV('%obj_file_name + ', '.join(str(x) for x in i) + ');\n')
-    for i in uvface:
-        f.write('    %s.addFaceUV('%obj_file_name + ', '.join(str(x) for x in i) + ');\n')
+    if len(uvface) > 0:
+        f.write('    %s.uv_vertexs.reserve('%obj_file_name + str(len(uvvertex)) + ');\n')
+        f.write('    %s.uv_faces.reserve('%obj_file_name + str(len(uvface)) + ');\n')
+        for i in uvvertex:
+            f.write('    %s.addVertexUV('%obj_file_name + ', '.join(str(x) for x in i) + ');\n')
+        for i in uvface:
+            f.write('    %s.addFaceUV('%obj_file_name + ', '.join(str(x) for x in i) + ');\n')
+    
+    f.write('#endif\n')
     f.write('}\n')
+    
+    
 
 
 
