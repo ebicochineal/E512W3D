0c1f59699c0bb3334097d0930c5697ccb525f663
ebicochineal <yz94588@gmail.com>
2023-07-15 22:49:36 +0900

    update

--- examples/example4/E512W3DWindow.hpp
+++ examples/example4/E512W3DWindow.hpp
@@ -229,6 +229,7 @@ public:
         }
     }
     
+    
     void drawCircle (int16_t sx, int16_t sy, int16_t ex, int16_t ey, uint16_t color = 0xFFFF) {
         if (this->dsy == this->dey || this->dsx == this->dex) { return; }
         
@@ -272,14 +273,52 @@ public:
         ex += this->sx;
         ey += this->sy;
         
-        int16_t size = ex-sx;
-        ex = min(ex, dex);
-        ey = min(ey, dey);
-        sx = max(sx, dsx);
-        sy = max(sy, dsy);
+        ex = min(ex, this->dex);
+        ey = min(ey, this->dey);
+        sx = max(sx, this->dsx);
+        sy = max(sy, this->dsy);
         for (int16_t y = sy; y < ey; ++y) {
             for (int16_t x = sx; x < ex; ++x) {
                 this->buff->drawPixel(x, y, color);
+            }
+        }
+    }
+    
+    void drawTexture (int16_t sx, int16_t sy, Texture& tex, bool flipx = false) {
+        if (this->dsy == this->dey || this->dsx == this->dex) { return; }
+        
+        sx += this->sx;
+        sy += this->sy;
+        int16_t ex = sx + tex.width;
+        int16_t ey = sy + tex.height;
+        
+        ex = min(ex, dex);
+        ey = min(ey, dey);
+        int16_t isx = max(sx, dsx);
+        int16_t isy = max(sy, dsy);
+        int16_t itx = sx < dsx ? dsx - sx : 0;
+        int16_t ity = sy < dsy ? dsy - sy : 0;
+        if (flipx) {
+            for (int16_t y = isy, ty = ity; y < ey; ++y, ++ty) {
+                const size_t tyy = ty * tex.width;
+                for (int16_t x = isx, tx = itx; x < ex; ++x, ++tx) {
+                    const uint16_t c1555 = tex.pixels[tyy + tex.width-tx-1];
+                    const uint16_t r = ((c1555 >> 10) & 0b11111) << 11;
+                    const uint16_t g = ((c1555 >>  5) & 0b11111) <<  6;
+                    const uint16_t b = ((c1555      ) & 0b11111)      ;
+                    if ((c1555 >> 15 & 1) == 0) { this->buff->drawPixel(x, y, r|g|b); }
+                }
+            }
+        } else {
+            for (int16_t y = isy, ty = ity; y < ey; ++y, ++ty) {
+                const size_t tyy = ty * tex.width;
+                for (int16_t x = isx, tx = itx; x < ex; ++x, ++tx) {
+                    const uint16_t c1555 = tex.pixels[tyy + tx];
+                    const uint16_t r = ((c1555 >> 10) & 0b11111) << 11;
+                    const uint16_t g = ((c1555 >>  5) & 0b11111) <<  6;
+                    const uint16_t b = ((c1555      ) & 0b11111)      ;
+                    if ((c1555 >> 15 & 1) == 0) { this->buff->drawPixel(x, y, r|g|b); }
+                }
             }
         }
     }
